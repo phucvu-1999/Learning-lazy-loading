f6470b769935515e1a24020fae69e798989d35a0
Tien Phuc <leesinvn123@gmail.com>
2022-04-17 21:56:05 +0700

    Learning Intersection Observer

--- script.js
+++ script.js
@@ -93,12 +93,31 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky navigation
-const initialCords = section1.getBoundingClientRect();
+// const initialCords = section1.getBoundingClientRect();
 
-window.addEventListener('scroll', function (e) {
-  if (window.scrollY > initialCords.top) nav.classList.add('sticky');
+// window.addEventListener('scroll', function (e) {
+//   if (window.scrollY > initialCords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+// Sticky navigation: Intersection observer
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries, callback) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
 
 // // Event propagation
 // const randomInt = (min, max) =>
